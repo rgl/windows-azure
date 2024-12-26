513eb052defbe4b9f845e35f43ec0805b0465a46
Rui Lopes <rgl@ruilopes.com>
2024-12-26 19:50:24 +0000

    verify the sysprep execution

--- provision-generalize.ps1
+++ provision-generalize.ps1
@@ -19,15 +19,34 @@ Stop-Service sshd
 Remove-Item -Force C:\ProgramData\ssh\ssh_host_*
 
 # sysprep the machine.
-# see https://learn.microsoft.com/en-us/windows-hardware/manufacture/desktop/windows-setup-states?view=windows-10
+# see https://github.com/rgl/windows-sysprep-playground
+# see https://learn.microsoft.com/en-us/windows-hardware/manufacture/desktop/windows-setup-states?view=windows-11
+# NB the logs are stored in the following directories:
+#         generalize phase: C:\Windows\System32\Sysprep\Panther
+#         specialize phase: C:\Windows\Panther
+#         oobe phase:       C:\Windows\Panther\UnattendGC
 Write-Host 'Syspreping the machine...'
-& "$env:SystemRoot\System32\Sysprep\Sysprep.exe" /oobe /generalize /quiet /quit
-while ($true) {
-    $imageState = (Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Setup\State).ImageState
-    Write-Output $imageState
-    if ($imageState -ne 'IMAGE_STATE_GENERALIZE_RESEAL_TO_OOBE') {
-        Start-Sleep -Seconds 10
-    } else {
-        break
-    }
+$sysprepSucceededTagPath = 'C:\Windows\System32\Sysprep\Sysprep_succeeded.tag'
+# NB although sysprep is supposed to delete this, to be safe (e.g. earlier
+#    sysprep errors), delete it.
+if (Test-Path $sysprepSucceededTagPath) {
+    Remove-Item -Force $sysprepSucceededTagPath
+}
+C:\Windows\System32\Sysprep\Sysprep.exe `
+    /generalize `
+    /oobe `
+    /quiet `
+    /quit `
+    | Out-String -Stream
+
+Write-Host 'Checking for sysprep errors...'
+if (!(Test-Path $sysprepSucceededTagPath)) {
+    Get-Content C:\Windows\System32\Sysprep\Panther\setuperr.log
+    throw "sysprep failed because no $sysprepSucceededTagPath file was found. for more details see the C:\Windows\System32\Sysprep\Panther\setupact.log file (and related files)."
+}
+
+Write-Host 'Ensuring the windows image state is IMAGE_STATE_GENERALIZE_RESEAL_TO_OOBE...'
+$imageState = (Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Setup\State).ImageState
+if ($imageState -ne 'IMAGE_STATE_GENERALIZE_RESEAL_TO_OOBE') {
+    throw "the windows image state $imageState is not the expected IMAGE_STATE_GENERALIZE_RESEAL_TO_OOBE."
 }
